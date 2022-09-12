0400686761806e4c1097c2a09ab69c4c500a1994
Abir <majumdarabir3@gmail.com>
2022-09-12 22:28:27 +0530

    finishing the product page

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^_g%33qd(g8bjc+*40&uh(ptgkb$&-*+0!i3$lu7xj1u166cbb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- store/models/product.py
+++ store/models/product.py
@@ -4,6 +4,7 @@ from .category import Category
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
+    quantity = models.CharField(max_length=50, null=True, blank=True)
     price = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description = models.CharField(
