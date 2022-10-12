f56539d5aa3b38e9a454ddfc04972e644521e352
tuuhin <tuhinbhowmick2513@gmail.com>
2022-10-13 01:48:01 +0530

    full app names are given to django apps Such that django signals can work Admin site header is changed. base path `<url>` is set to store app

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -28,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,10 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base',
-    'store',
-    'cart',
-    'products',
+    'base.apps.BaseConfig',
+    'store.apps.StoreConfig',
+    'cart.apps.CartConfig',
+    'products.apps.ProductsConfig',
     "crispy_forms",
     "crispy_bootstrap5",
 ]
@@ -133,6 +131,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/auth/login'
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
--- Eshop/urls.py
+++ Eshop/urls.py
@@ -2,13 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from Eshop import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+
 
 urlpatterns = [
+    path('', include('store.urls')),
     path('admin/', admin.site.urls),
     path('auth/', include('base.urls')),
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
-    path('store/', include('store.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+admin.site.site_header = "DJANGO_MEDIC_SHOP".title()
